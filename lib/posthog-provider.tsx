1d1b3292097f6875bdf178d6459b8087b6c86d86
Anthony Villazana <63469220+antguivy@users.noreply.github.com>
2026-02-28 08:29:45 -0500

    Update posthog-provider.tsx (#64)

--- lib/posthog-provider.tsx
+++ lib/posthog-provider.tsx
@@ -2,9 +2,19 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "/ingest",
+    capture_pageview: false,
+    capture_pageleave: true,
+    session_recording: {
+      maskAllInputs: true,
+    },
+  });
+}
 
 function PostHogPageView() {
   const pathname = usePathname();
@@ -25,18 +35,6 @@ function PostHogPageView() {
 }
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
-      capture_pageview: false,
-      capture_pageleave: true,
-      session_recording: {
-        maskAllInputs: true,
-      }
-    });
-  }, []);
-
   return (
     <PHProvider client={posthog}>
       <Suspense fallback={null}>
